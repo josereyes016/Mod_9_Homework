0dd41e873e8e2d86efd3ff4c6c0747f941cc831e
Jose <codesmash50@gmail.com>
2017-11-26 19:28:32 -0500

    Added Student Class

--- Mod_9_Homework/Student.cs
+++ Mod_9_Homework/Student.cs
@@ -8,5 +8,12 @@ namespace Mod_9_Homework
 {
     class Student
     {
+        private string firstName;
+        private string lastName;
+        private string birthday;
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Birthday { get; set; }
     }
 }
